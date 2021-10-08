677a46779b052cf4524ca8466e64d9450cc1c405
ryan362 <59591086+ryan362@users.noreply.github.com>
2021-10-08 14:22:46 -0400

    first

---
+++ read.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+
+img = cv.imread('Resources/Photos/cat_large.jpg')
+cv.imshow('Cat', img)
+
+# Reading videos
+# capture = cv.VideoCapture('Resources/Videos/dog.mp4')
+
+# while True:
+#     isTrue, frame = capture.read()
+#     cv.imshow('Video', frame)
+
+#     if cv.waitKey(20) & 0xFF == ord('d'):
+#         break
+
+# capture.release()
+# cv.destroyAllWindows()
+
+
+cv.waitKey(0)
